9191e04d0ab945408da510ecb13f54d3886b3aa6
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-02-26 19:10:37 +0530

    API added

---
+++ RestService.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+weather = {
+    "data": [
+        {
+            "day": "1/6/2019",
+            "temperature": "23",
+            "windspeed": "16",
+            "event": "Sunny"
+        },
+        {
+            "day": "2/6/2019",
+            "temperature": "21",
+            "windspeed": "18",
+            "event": "Rainy"
+        },
+        {
+            "day": "3/6/2019",
+            "temperature": "31",
+            "windspeed": "12",
+            "event": "Sunny"
+        },
+        {
+            "day": "4/6/2019",
+            "temperature": "5",
+            "windspeed": "28",
+            "event": "Snow"
+        },
+        {
+            "day": "5/6/2019",
+            "temperature": "17",
+            "windspeed": "18",
+            "event": "Rainy"
+        },
+        {
+            "day": "6/6/2019",
+            "temperature": "19",
+            "windspeed": "21",
+            "event": "Rainy"
+        },
+        {
+            "day": "7/6/2019",
+            "temperature": "28",
+            "windspeed": "14",
+            "event": "Sunny"
+        }
+    ]
+}
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return "Welcome to Flask"
+
+
+@app.route("/weatherReport/", methods=['GET'])
+def WeatherReport():
+    global weather
+    return jsonify([weather])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
