2c0055f44ec5338ff53278e45bd6431954bc5957
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-03-11 09:53:17 +0530

    change

--- cbot_response.py
+++ cbot_response.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from text_lemmatizer import LemNormalize
 
 from text_lemmatizer import LemNormalize
 
